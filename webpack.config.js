4130db38e6d2599155d75b3ccbceeabe611f8d30
sysadmin <petelelliott@gmail.com>
2017-10-04 18:24:18 +0100

    added dist dir and added bundle.js. Still need to streamline build process from config file

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,21 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
+  template: './dist/index.html',
   filename: 'index.html',
   inject: 'body'
 })
 
+
+
+
+
 module.exports = { 
   entry: './src/index.js', 
-  output: { 
-    path: path.resolve('dist'), 
-    filename: 'index_bundle.js'
-  }, 
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: { 
     loaders: [ 
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
