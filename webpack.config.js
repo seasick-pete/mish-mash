aa9f7df76d16485c64ed1b2663f531cfa3dc88d7
sysadmin <petelelliott@gmail.com>
2017-10-04 21:04:25 +0100

    updated dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   entry: './src/index.js', 
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'docs')
   },
   module: { 
     loaders: [ 
