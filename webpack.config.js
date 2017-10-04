6e97f1c6478b312865052b3044cfa17eb6dcb679
sysadmin <petelelliott@gmail.com>
2017-10-04 18:58:20 +0100

    hooked up bundle

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './index.html',
+  template: './src/index.html',
   filename: 'index.html',
   inject: 'body'
 })
